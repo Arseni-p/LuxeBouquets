285db5a3708d0b9a1b5b908e9b20e4aa2f355a7f
Arseni-p <arsenipantsialei@gmail.com>
2024-01-16 16:40:33 +0100

    edited common files

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,31 @@
 'use strict';
-// import * as fn from "./functions/functions.js"
-// import * as md from "./modules/modules.js";
+import * as fn from "./functions/functions.js"
 
+let reviewsListWrapper = document.querySelector('.reviews__list--wrapper');
+// if (!reviewsListWrapper) reviewsListWrapper = fn.getReviewsListWrapper();
+
+document.addEventListener('DOMContentLoaded', () => {
+  fn.createReviewsDrops(reviewsListWrapper);
+});
+
+document.addEventListener('click', (event) => {
+  const btn = event.target.closest('.btn');
+  console.log(btn, event.target.classList);
+  if (
+    btn && btn.classList.contains('popup__enable') || 
+    event.target.classList.contains('info__wrapper') ||
+    btn && btn.classList.contains('info__btn--disable')) fn.getInfo(reviewsListWrapper);
+})
+
+reviewsListWrapper.addEventListener('click', (event) => {
+  let arrowBtn = 'arrowLeft';
+  const reviewsBtnLeft = document.querySelector('.arrow-left');
+  const reviewsBtnRight = document.querySelector('.arrow-right');
+  if (event.target === reviewsBtnRight) arrowBtn = 'arrowRight';
+  if (event.target === reviewsBtnLeft || event.target === reviewsBtnRight) fn.reviewsSlide(reviewsListWrapper, arrowBtn);
+  if (event.target.classList.contains('reviews__dots--item')) fn.getReviewByDot(reviewsListWrapper, event);
+})
+
+window.addEventListener('resize', () => {
+  fn.resizeWindow(reviewsListWrapper);
+})
